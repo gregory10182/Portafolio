9739959452fcbfd3360490e19e5e005ab2236f3f
gregory10182 <Gregory10183@gmail.com>
2023-01-31 14:50:27 -0300

    Organize projects section

--- portafolio/src/components/projects.jsx
+++ portafolio/src/components/projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
+import Technology from "./technology"
 import "../css/Projects.css";
 
 export default function Projects() {
@@ -9,29 +10,92 @@ export default function Projects() {
       <div className="Project">
         <h2>Recetario Diana Cocina</h2>
         <p>
-          El recetario fue desarrollado para solucionar un problema muy
-          recurrente en las personas que les gusta cocinar y es el estandarizar,
-          almacenar y ajustar las cantidades de los ingredientes segun lo que se
-          desee cocinar.
+          El recetario fue desarrollado como herramienta de ayuda para las
+          personas que les gusta cocinar y con el cual pueden estandarizar,
+          almacenar y ajustar las cantidades de los ingredientes segun la
+          cantidad que se desee cocinar.
         </p>
+        <div className="Technologies">
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+        </div>
+        <div className="Links">
+          <img src="https://cdn-icons-png.flaticon.com/512/1240/1240971.png" alt="" />
+          <img src="https://cdn-icons-png.flaticon.com/512/455/455590.png" alt="" />
+        </div>
       </div>
       <div className="Project">
         <h2>Recetario Diana Cocina</h2>
         <p>
-          El recetario fue desarrollado para solucionar un problema muy
-          recurrente en las personas que les gusta cocinar y es el estandarizar,
-          almacenar y ajustar las cantidades de los ingredientes segun lo que se
-          desee cocinar.
+          El recetario fue desarrollado como herramienta de ayuda para las
+          personas que les gusta cocinar y con el cual pueden estandarizar,
+          almacenar y ajustar las cantidades de los ingredientes segun la
+          cantidad que se desee cocinar.
         </p>
+        <div className="Technologies">
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+        </div>
+        <div className="Links">
+          <img src="https://cdn-icons-png.flaticon.com/512/1240/1240971.png" alt="" />
+          <img src="https://cdn-icons-png.flaticon.com/512/455/455590.png" alt="" />
+        </div>
       </div>
       <div className="Project">
         <h2>Recetario Diana Cocina</h2>
         <p>
-          El recetario fue desarrollado para solucionar un problema muy
-          recurrente en las personas que les gusta cocinar y es el estandarizar,
-          almacenar y ajustar las cantidades de los ingredientes segun lo que se
-          desee cocinar.
+          El recetario fue desarrollado como herramienta de ayuda para las
+          personas que les gusta cocinar y con el cual pueden estandarizar,
+          almacenar y ajustar las cantidades de los ingredientes segun la
+          cantidad que se desee cocinar.
         </p>
+        <div className="Technologies">
+          <Technology
+            source="https://img.icons8.com/ultraviolet/2x/react--v2.png"
+            tech="React"
+          />
+          <Technology
+            source="https://img.icons8.com/office/2x/express-js.png"
+            tech="Express"
+          />
+          <Technology
+            source="https://img.icons8.com/external-tal-revivo-color-tal-revivo/2x/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png"
+            tech="MongoDB"
+          />
+        </div>
+
+        <div className="Links">
+          <img src="https://cdn-icons-png.flaticon.com/512/1240/1240971.png" alt="" />
+          <img src="https://cdn-icons-png.flaticon.com/512/455/455590.png" alt="" />
+        </div>
       </div>
     </motion.div>
   );
